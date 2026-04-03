e9daee3f4ac03d39224b08f714f2025b34abc10f
hfoster1 <127918381+hfoster1@users.noreply.github.com>
2026-04-03 10:25:54 +1000

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SaaCourse from './SaaCourse.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SaaCourse />
-  </React.StrictMode>,
-)
