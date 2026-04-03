6aa54708320e06298d507314687edbff363ae30f
hfoster1 <127918381+hfoster1@users.noreply.github.com>
2026-04-03 10:24:53 +1000

    Set up React application with SaaCourse component

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SaaCourse from './SaaCourse.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SaaCourse />
+  </React.StrictMode>,
+)
